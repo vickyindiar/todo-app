a57323ac790f80619a9e289013ba90773450d0da
oji <sifauzi@gmail.com>
2018-12-18 13:26:56 +0700

    Add component dir

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Todo from './Todo';
+import Todo from './Components/Todo';
 
 import * as serviceWorker from './serviceWorker';
 
